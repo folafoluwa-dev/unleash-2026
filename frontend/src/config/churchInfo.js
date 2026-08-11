d47dcab191af95b90860ac0db4cbf15b0b3866f6
ayfola <ayfola14@gmailcom>
2026-08-12 00:07:01 +0100

    test 1

--- frontend/src/config/churchInfo.js
+++ frontend/src/config/churchInfo.js
@@ -5,9 +5,9 @@ const churchInfo = {
   email: "",          // add when available, e.g. "info@loveofchristchapel.org"
   website: "",        // add when available
   socials: {
-    facebook: "",     // e.g. "https://facebook.com/loveofchristchapel"
-    instagram: "",    // e.g. "https://instagram.com/loveofchristchapel"
-    youtube: "",      // e.g. "https://youtube.com/@loveofchristchapel"
+    facebook: "https://web.facebook.com/people/Locci-Kings-court-assembly-new/100079756581591/#",     // e.g. "https://facebook.com/loveofchristchapel"
+    instagram: "https://www.instagram.com/kingscourtnew/",    // e.g. "https://instagram.com/loveofchristchapel"
+    youtube: "https://www.youtube.com/@loccikingscourt3765",      // e.g. "https://youtube.com/@loveofchristchapel"
   },
 };
 
